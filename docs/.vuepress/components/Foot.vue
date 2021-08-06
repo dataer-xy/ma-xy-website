cde9f3bba1d14b81ca99d7c8d9c801bfa0e0034c
songyanyi <1121225022@qq.com>
2021-08-06 20:40:32 +0800

    添加公众号

--- docs/.vuepress/components/Foot.vue
+++ docs/.vuepress/components/Foot.vue
@@ -1,9 +1,19 @@
 <template>
 
     <div class="footer">
+        
+        <div>
+            <img src="/公众号.jpg" alt="文档伴侣公众号" style="width:128px;height:128px">
+        </div>
+
+        <div>
+
+            扫码关注微信公众号 `文档伴侣`，关注更多的文章和进展。
+        </div>
+        
         Copyright © 2020 ma-xy.com
         <br>
-        站长： 王源震(QQ：2432744025)，合作等请联系站长
+        站长： 王(QQ：2432744025)，合作等请联系站长
     </div>
 </template>
 <script>
